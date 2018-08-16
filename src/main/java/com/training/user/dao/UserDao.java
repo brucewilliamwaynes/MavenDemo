7afc735da7028104c014539666ea4cf7a8ddba10
brucewilliamwaynes <mukherjee.anirbanmukherjee@gmail..com>
2018-08-16 15:33:26 +0000

    DAO Completed

---
+++ src/main/java/com/training/user/dao/UserDao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.training.user.dao;
+
+import com.training.user.model.Login;
+import com.training.user.model.User;
+
+/**
+ * @author bridgelabz
+ *
+ */
+public interface UserDao {
+
+	void register(User user);
+	
+	  User validateUser(Login login);
+	
+}
